b482fceb73c9c4a45994cac33b65e6c77a638e23
Matithyahu <matthew@metzger.cc>
2017-06-09 20:21:30 -0500

    wrote my own function for displayTodos

--- script.js
+++ script.js
@@ -1,19 +1,28 @@
 var todoList = {
   todos: [],
   displayTodos: function() {
-    console.log("My Todos: ", this.todos);
+    var loopTodos = function() {
+      for (var i = 0; i < todoList.todos.length; i++){
+        console.log(" * " + todoList.todos[i].todoText );
+        }
+    }
+    console.log("My Todos: ");
+    loopTodos();
   },
+
   addTodo: function(todoText) {
     this.todos.push({
       todoText: todoText,
       completed: false
     });
+    // this.displayTodos();
+  },
+
+  changeTodo: function(position, todoText) {
+    this.todos[position].todoText = todoText;
     this.displayTodos();
   },
-  changeTodo: function(position, newValue) {
-    this.todos[position] = newValue;
-    this.displayTodos();
-  },
+
   deleteTodo: function(position) {
     this.todos.splice(position, 1);
     this.displayTodos();
